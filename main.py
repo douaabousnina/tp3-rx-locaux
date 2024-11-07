b3972cb0644a8cf1a88dcf572aa58df78e3b95ce
do3 <bousnina.douaa1@gmail.com>
2024-11-07 09:31:55 +0100

    tache 2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import subprocess
 import re
 import platform
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def calculate_distance_mobile_AP(rssi, measured_power=-50, environmental_factor=3):
@@ -38,6 +41,52 @@ def get_wifi_rssi():
     else:
         raise Exception('Unsupported OS')
         
+#tache 2
+# Position des points d'accès (à définir selon votre scénario)
+AP1 = np.array([2, 2])
+AP2 = np.array([8, 2])
+AP3 = np.array([5, 6])
+
+# RSSI mesurés (exemple, à remplacer par vos valeurs réelles)
+rssi1 = -60  # RSSI à partir du point d'accès 1
+rssi2 = -65  # RSSI à partir du point d'accès 2
+rssi3 = -60  # RSSI à partir du point d'accès 3
+
+d1 = calculate_distance_mobile_AP(rssi1)
+d2 = calculate_distance_mobile_AP(rssi2)
+d3 = calculate_distance_mobile_AP(rssi3)
+
+# Affichage graphique des cercles de distance
+fig, ax = plt.subplots()
+
+# Tracer les cercles
+circle1 = plt.Circle(AP1, d1, color='r', fill=False, linestyle='dotted', label="AP1")
+circle2 = plt.Circle(AP2, d2, color='g', fill=False, linestyle='dotted', label="AP2")
+circle3 = plt.Circle(AP3, d3, color='b', fill=False, linestyle='dotted', label="AP3")
+
+ax.add_artist(circle1)
+ax.add_artist(circle2)
+ax.add_artist(circle3)
+
+# Tracer les points d'accès
+ax.plot(AP1[0], AP1[1], 'ro', label="Point d'accès 1")
+ax.plot(AP2[0], AP2[1], 'go', label="Point d'accès 2")
+ax.plot(AP3[0], AP3[1], 'bo', label="Point d'accès 3")
+
+# Paramétrage du graphique
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_aspect('equal', 'box')
+ax.legend()
+
+# Affichage du graphique
+plt.title("Position estimée du mobile")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+
+
 
 
 wifi_rssi = get_wifi_rssi()
